5f3458be3e84d08d17946fbda3f6f539fca71833
Oliver Loeffler <oliver@raumzeitfalle.de>
2016-11-07 22:02:10 +0100

    initial commit.

---
+++ ApacheIvy/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+$ErrorActionPreference = 'Stop'
+ 
+$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+ 
+$packageArgs = @{
+  packageName   = 'ivy'
+  unzipLocation = $toolsDir
+  url           = 'ftp://ftp.fu-berlin.de/unix/www/apache//ant/ivy/2.4.0/apache-ivy-2.4.0-bin.zip'
+  softwareName  = 'ivy*'
+  checksum      = '86b581c7be6358e7e98ec76968d9aac8'
+  checksumType  = 'md5'
+}
+ 
+if(!(Test-Path $env:ANT_HOME)) {
+  $ex = New-Object System.Exception "This package requires that the ANT_HOME environment variable is set to the ant home directory. Please set the environment variable, reload your console, and try installing this package again."
+  throw $ex
+}
+ 
+Install-ChocolateyZipPackage @packageArgs
+Copy-Item ($toolsDir + "\apache-ivy-2.4.0\ivy-2.4.0.jar") ($env:ANT_HOME + "\lib\ivy-2.4.0.jar")
+
+refreshenv
---
+++ ApacheIvy/tools/chocolateyUninstall.ps1
@@ -0,0 +1,6 @@
+$ErrorActionPreference = 'Stop'
+ 
+$packageName = 'ivy'
+ 
+Uninstall-ChocolateyZipPackage $packageName apache-ivy-2.4.0-bin.zip
+Remove-Item ($env:ANT_HOME + "\lib\ivy-2.4.0.jar")
