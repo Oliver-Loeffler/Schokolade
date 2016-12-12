660217c6fad6d32166c985ab921b50e6da9025ec
Oliver Loeffler <oliver@raumzeitfalle.de>
2016-12-12 20:01:19 +0100

    Redundant files removed.

--- Ivy/tools/chocolateyUninstall.ps1
+++
@@ -1,6 +0,0 @@
-$ErrorActionPreference = 'Stop'
- 
-$packageName = 'ApacheIvy'
- 
-Uninstall-ChocolateyZipPackage $packageName apache-ivy-2.4.0-bin.zip
-Remove-Item ($env:ANT_HOME + "\lib\ivy-2.4.0.jar")
