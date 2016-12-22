216bafa1b5528789aa8e2c76ee37efa063fe9121
Oliver Löffler <Oliver-Loeffler@users.noreply.github.com>
2016-12-22 08:40:55 +0100

    Update to 4.6.2 (Neon2)

--- EclipseJavaSE/tools/chocolateyInstall.ps1
+++ EclipseJavaSE/tools/chocolateyInstall.ps1
@@ -1,15 +1,15 @@
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
-$eclipse_home = Join-Path $package 'eclipse-4.6.1'
+$eclipse_home = Join-Path $package 'eclipse-4.6.2'
 $eclipse_exe = Join-Path $eclipse_home 'eclipse/eclipse.exe'
 
 
 $packageArgs = @{
   packageName   = 'EclipseJavaSE'
   unzipLocation = $package
-  url           = 'https://ftp.fau.de/eclipse/technology/epp/downloads/release/neon/1a/eclipse-java-neon-1a-win32-x86_64.zip'
+  url           = 'https://ftp.fau.de/eclipse/technology/epp/downloads/release/neon/2/eclipse-java-neon-2-win32-x86_64.zip'
   softwareName  = 'Eclipse.org Eclipse IDE for Java'
-  checksum      = 'bacc358c2587c2b084435486780adea2eae8796c395752a72e8786115747dcadf3e40149a7e0b40aba45fea640e8434ce9f172c1c4be1c99e7afd0c15edcc4c9'
+  checksum      = '4a225fc5a35fb9097cde8c5a2e954d5a03104ec014e34ba8585b3b4a9395932d0641ee57c98ce502dfe9012080a43d5bd1bc326259b31b1a56e75ba7987789e2'
   checksumType  = 'sha512'
 }
  
