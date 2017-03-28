1d3967fb34add2aaf493f48cf13fbe4c9c2f302a
Oliver Loeffler <oliver@raumzeitfalle.de>
2017-03-28 22:40:04 +0200

    Eclipse for JavaSE - NEON3 added.

--- EclipseJavaSE/tools/chocolateyInstall.ps1
+++ EclipseJavaSE/tools/chocolateyInstall.ps1
@@ -1,15 +1,15 @@
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
-$eclipse_home = Join-Path $package 'eclipse-4.6.2'
+$eclipse_home = Join-Path $package 'eclipse-4.6.3'
 $eclipse_exe = Join-Path $eclipse_home 'eclipse/eclipse.exe'
 
 
 $packageArgs = @{
   packageName   = 'EclipseJavaSE'
   unzipLocation = $package
-  url           = 'https://ftp.fau.de/eclipse/technology/epp/downloads/release/neon/2/eclipse-java-neon-2-win32-x86_64.zip'
+  url           = 'https://ftp.fau.de/eclipse/technology/epp/downloads/release/neon/3/eclipse-java-neon-3-win32-x86_64.zip'
   softwareName  = 'Eclipse.org Eclipse IDE for Java'
-  checksum      = '4a225fc5a35fb9097cde8c5a2e954d5a03104ec014e34ba8585b3b4a9395932d0641ee57c98ce502dfe9012080a43d5bd1bc326259b31b1a56e75ba7987789e2'
+  checksum      = 'd1c6111bad30b5aeb6253fe2fbac636b6b35abb5b554455ed3a89061331cd30812420b0516151ef813b9cf89b7b498e707964ced6d5354c8b754d4258eead668'
   checksumType  = 'sha512'
 }
  
--- EclipseJavaSE/tools/chocolateyUninstall.ps1
+++ EclipseJavaSE/tools/chocolateyUninstall.ps1
@@ -2,8 +2,8 @@ $ErrorActionPreference = 'Stop'
 $packageName = 'EclipseJavaSE'
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
-$eclipse_home = Join-Path $package 'eclipse-4.6.2'
+$eclipse_home = Join-Path $package 'eclipse-4.6.3'
 $eclipse_exe = Join-Path $eclipse_home 'eclipse/eclipse.exe'
 
 Uninstall-BinFile -Name 'eclipse' -Path $eclipse_exe
-Uninstall-ChocolateyZipPackage $packageName eclipse-java-neon-2-win32-x86_64.zip
+Uninstall-ChocolateyZipPackage $packageName eclipse-java-neon-3-win32-x86_64.zip
