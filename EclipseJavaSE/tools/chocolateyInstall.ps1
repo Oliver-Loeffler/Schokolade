9113674a4c826c6113e64ed55875bb9d10dc124b
Oliver Loeffler <oliver@raumzeitfalle.de>
2017-07-12 22:13:48 +0200

    Eclipse for Java SE updated - Oxygen

--- EclipseJavaSE/tools/chocolateyInstall.ps1
+++ EclipseJavaSE/tools/chocolateyInstall.ps1
@@ -1,18 +1,18 @@
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
-$eclipse_home = Join-Path $package 'eclipse-4.6.3'
+$eclipse_home = Join-Path $package 'eclipse-4.7'
 $eclipse_exe = Join-Path $eclipse_home 'eclipse/eclipse.exe'
 
 
 $packageArgs = @{
   packageName   = 'EclipseJavaSE'
   unzipLocation = $package
-  url           = 'https://ftp.fau.de/eclipse/technology/epp/downloads/release/neon/3/eclipse-java-neon-3-win32-x86_64.zip'
+  url           = 'https://ftp.fau.de/eclipse/technology/epp/downloads/release/oxygen/R/eclipse-java-oxygen-R-win32-x86_64.zip'
   softwareName  = 'Eclipse.org Eclipse IDE for Java'
-  checksum      = 'd1c6111bad30b5aeb6253fe2fbac636b6b35abb5b554455ed3a89061331cd30812420b0516151ef813b9cf89b7b498e707964ced6d5354c8b754d4258eead668'
+  checksum      = 'aa41bced7699b933fac9b254de1609edfec7f2baac89a5a262e92d9119561a6b19fb367a65946ced7ab718f640b74b51f753ed685aeeb45b50f533e6131ac85a'
   checksumType  = 'sha512'
 }
- 
+  
 Install-ChocolateyZipPackage @packageArgs
 Install-BinFile -Name 'eclipse' -Path $eclipse_exe
 
