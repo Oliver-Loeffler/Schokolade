5183363da7158e258b3e9740093e51e652913cd9
Oliver Loeffler <oliver@raumzeitfalle.de>
2020-02-16 21:40:08 +0100

    Updated to Eclipse 4.7.1

--- EclipseJavaSE/tools/chocolateyInstall.ps1
+++ EclipseJavaSE/tools/chocolateyInstall.ps1
@@ -1,15 +1,15 @@
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
-$eclipse_home = Join-Path $package 'eclipse-4.7'
+$eclipse_home = Join-Path $package 'eclipse-4.7.1'
 $eclipse_exe = Join-Path $eclipse_home 'eclipse/eclipse.exe'
 
 
 $packageArgs = @{
   packageName   = 'EclipseJavaSE'
   unzipLocation = $package
-  url           = 'https://ftp.fau.de/eclipse/technology/epp/downloads/release/oxygen/R/eclipse-java-oxygen-R-win32-x86_64.zip'
+  url           = 'http://ftp-stud.fht-esslingen.de/pub/Mirrors/eclipse/technology/epp/downloads/release/oxygen/1a/eclipse-java-oxygen-1a-win32-x86_64.zip'
   softwareName  = 'Eclipse.org Eclipse IDE for Java'
-  checksum      = 'aa41bced7699b933fac9b254de1609edfec7f2baac89a5a262e92d9119561a6b19fb367a65946ced7ab718f640b74b51f753ed685aeeb45b50f533e6131ac85a'
+  checksum      = '9E5CC14236DB2A7712EF18D5DDC636A863F2DB6BA0A9E7F266353698369D1232D5FF6903001C3E761A661B6DDB3DF06DA3F69EEC7EA205D3F59F1B055C024077'
   checksumType  = 'sha512'
 }
   
--- EclipseJavaSE/tools/chocolateyUninstall.ps1
+++ EclipseJavaSE/tools/chocolateyUninstall.ps1
@@ -2,8 +2,8 @@ $ErrorActionPreference = 'Stop'
 $packageName = 'EclipseJavaSE'
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
-$eclipse_home = Join-Path $package 'eclipse-4.7'
+$eclipse_home = Join-Path $package 'eclipse-4.7.1'
 $eclipse_exe = Join-Path $eclipse_home 'eclipse/eclipse.exe'
 
 Uninstall-BinFile -Name 'eclipse' -Path $eclipse_exe
-Uninstall-ChocolateyZipPackage $packageName eclipse-java-oxygen-R-win32-x86_64.zip
+Uninstall-ChocolateyZipPackage $packageName eclipse-java-oxygen-1a-win32-x86_64.zip
